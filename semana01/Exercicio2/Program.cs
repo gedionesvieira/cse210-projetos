dbcc981587d1620226ecf192fcafdb2982fc4408
gedionesvieira <gvieira2@byupathway.edu>
2026-09-05 12:46:44 -0400

    Alterado

--- semana01/Exercicio2/Program.cs
+++ semana01/Exercicio2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio2.");
+        Console.WriteLine("Qual o percentual da sua nota? ");
+        string resposta = Console.ReadLine();
+        int percentual = int.Parse(resposta);
+
+        string letra = "";
+
+        if (percentual >= 90)
+        {
+            letra = "A";
+        }
+        else if (percentual >= 80)
+        {
+            letra = "B";
+        }
+        else if (percentual >= 70)
+        {
+            letra = "C";
+        }
+        else if (percentual >= 60)
+        {
+            letra = "D";
+        }
+        else
+        {
+            letra = "F";
+        }
+        Console.WriteLine($"Sua letra é: {letra}");
+
+        if (percentual >= 70)
+        {
+            Console.WriteLine("Você passou!");
+        }
+        else
+        {
+            Console.WriteLine("Mais sorte na proxima vez!");
+        }
     }
 }
